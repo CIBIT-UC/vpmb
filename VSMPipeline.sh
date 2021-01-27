9f8e2fd3e9351f75d23362c9b9971c92742d50e9
Alexandre Sayal <alexandresayal@gmail.com>
2021-01-27 19:07:16 +0000

    Update VSMPipeline.sh

--- VSMPipeline.sh
+++ VSMPipeline.sh
@@ -19,6 +19,7 @@ t1Dir="${VPDIR}/${subID}/ANALYSIS/T1W"                      # T1w directory
 WD="${VPDIR}/${subID}/ANALYSIS/${taskName}/FMAP-SPE/vsm"    # working directory
 ro_time=0.0415863 # in seconds
 #nThreads=36 # number of threads
+mniImage=$FSLDIR/data/standard/MNI152_T1_2mm
 
 # --------------------------------------------------------------------------------
 #  Create/Clean folder
@@ -42,7 +43,7 @@ cp $fmapDir/work/GREfromTOPUP-Phase.nii.gz $WD/fieldmap.nii.gz
 cp $fmapDir/work/spe-ap_dc_jac.nii.gz $WD/speReference.nii.gz
 
 # --------------------------------------------------------------------------------
-#  Calculate VDM
+#  Calculate VSM
 # --------------------------------------------------------------------------------
 
 fslmaths ${WD}/fieldmap \
@@ -53,7 +54,7 @@ fslmaths ${WD}/fieldmap \
 fsleyes $WD/fieldmap_vsm -dr -30000 30000 -cm brain_colours_diverging_bwr_iso &
 
 # --------------------------------------------------------------------------------
-#  Registration to MNI
+#  Estimate spe2mni
 # --------------------------------------------------------------------------------
 
 # BET speReference
@@ -62,45 +63,69 @@ mv $WD/speReferenceMask_mask.nii.gz $WD/speReference_brain_mask.nii.gz  # rename
 fslmaths $WD/speReference.nii.gz -mas $WD/speReference_brain_mask.nii.gz $WD/speReference_brain.nii.gz  # apply spe brain mask
 
 # check visually
-fsleyes $WD/speReference.nii.gz $WD/speReference_brain.nii.gz &
+fslview_deprecated $WD/speReference.nii.gz $WD/speReference_brain.nii.gz &
 
-# Affine guess
-flirt -ref $FSLDIR/data/standard/MNI152_T1_2mm_brain.nii.gz \
-      -in $WD/speReference_brain.nii.gz \
-      -out $WD/spe2mni_affineguess \
-      -omat $WD/spe2mni_affineguess.mat \
-      -dof 12 -v
-
-# check
-# fslview_deprecated $FSLDIR/data/standard/MNI152_T1_2mm_brain.nii.gz $WD/spe2mni_affineguess &
-
-# Non linear registration
-fnirt --ref=$FSLDIR/data/standard/MNI152_T1_2mm \
-    --refmask=$FSLDIR/data/standard/MNI152_T1_2mm_brain_mask \
-    --in=$WD/speReference.nii.gz \
-    --aff=$WD/spe2mni_affineguess \
-    --cout=$WD/spe2mni_coeff \
-    --fout=$WD/spe2mni_warp \
-    --iout=$WD/speReference2mni \
-    --warpres=10,10,10 -v
-
-# Check visually
-fslview_deprecated $FSLDIR/data/standard/MNI152_T1_2mm.nii.gz $WD/speReference2mni &
-
-# Alternative !
+# Bias field
+fast -B $WD/speReference_brain.nii.gz    # output: speReference_brain_restore
 
 # Estimate register from spe to struct
-flirt -ref ${t1Dir}/BET/${subDir}_T1W_brain \
-    -in $WD/speReference_brain \
-    -omat spe2struct.mat \
+flirt -ref ${t1Dir}/FAST/${subID}_T1W_brain_restore \
+    -in $WD/speReference_brain_restore \
+    -omat $WD/spe2struct \
+    -out $WD/spe2struct \
     -cost normmi \
     -interp sinc \
     -dof 6 -v
 
-# Combine spe2struct with struct2mni
-applywarp --ref=${FSLDIR}/data/standard/MNI152_T1_2mm \
-    --in=$WD/speReference \
-    --warp=${t1Dir}/MNI/struct2mni \
-    --premat=$WD/spe2struct.mat \
-    --out=$WD/speReference_MNI \
-    --interp=sinc
+# check visually
+fslview_deprecated ${t1Dir}/FAST/${subID}_T1W_brain_restore.nii.gz $WD/spe2struct.nii.gz &
+
+# Concatenate spe2struct and struct2mni_affine
+convert_xfm -omat $WD/spe2mni_affine \
+            -concat ${t1Dir}/MNI/struct2mni_affine.mat $WD/spe2struct
+
+# Apply spe2mni_affine
+flirt -ref $mniImage \
+    -in $WD/speReference \
+    -init $WD/spe2mni_affine \
+    -applyxfm \
+    -out $WD/speReference_MNI_affine \
+    -interp sinc -v
+
+# check
+fslview_deprecated $mniImage $WD/speReference_MNI_affine &
+
+# --------------------------------------------------------------------------------
+#  VSM to MNI
+# --------------------------------------------------------------------------------
+
+#fslmaths $WD/fieldmap_vsm -mas $WD/speReference_brain_mask.nii.gz $WD/fieldmap_vsm_brain  # apply spe brain mask
+
+# Apply spe2mni_affine
+flirt -ref $mniImage \
+    -in $WD/fieldmap_vsm \
+    -init $WD/spe2mni_affine \
+    -applyxfm \
+    -out $WD/fieldmap_vsm_MNI \
+    -interp sinc -v
+
+fslmaths $WD/fieldmap_vsm_MNI -mas ${mniImage}_brain_mask $WD/fieldmap_vsm_brain_MNI
+
+fsleyes ${mniImage} $WD/fieldmap_vsm_brain_MNI -dr -30000 30000 -cm brain_colours_diverging_bwr_iso &
+
+# # concatenate transformations spe2struct (linear) with struct2mni (nonlinear)
+# convertwarp --ref=$mniImage \
+#         --out=$WD/spe2mni \
+#         --premat=$WD/spe2struct \
+#         --warp1=${t1Dir}/MNI/struct2mni \
+#         --rel --verbose
+
+# # Apply spe2mni
+# applywarp --ref=$mniImage \
+#     --in=$WD/speReference \
+#     --warp=$WD/spe2mni \
+#     --out=$WD/speReference_MNI \
+#     --interp=sinc
+
+# # Check visually
+# fslview_deprecated $mniImage ${t1Dir}/MNI/${subID}_T1W_MNI $WD/speReference_MNI &
