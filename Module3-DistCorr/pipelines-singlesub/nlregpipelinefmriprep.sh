fd35fe08f256279f5e9143c08506db7077fd4fbe
Alexandre Sayal <alexandresayal@sim01.icnas.uc.pt>
2021-10-14 19:10:02 +0000

    nlreg stuff

---
+++ Module3-DistCorr/pipelines-singlesub/nlregpipelinefmriprep.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#bidsFolder="/SCRATCH/users/alexandresayal/test-vpmb-nlreg-fmriprep"
+#workFolder="/SCRATCH/users/alexandresayal/test-vpmb-nlreg-fmriprep-work"
+bidsFolder="/DATAPOOL/VPMB/VPMB-BIDS-NLREG"
+workFolder="/DATAPOOL/VPMB/VPMB-BIDS-NLREG-work"
+
+# Validate
+bids-validator $bidsFolder
+
+# fmriprep run
+fmriprep-docker $bidsFolder $bidsFolder/derivatives \
+    participant --participant-label 05 \
+    --work-dir $workFolder \
+    --fs-license-file /SCRATCH/software/freesurfer/license.txt \
+    --fs-no-reconall \
+    --stop-on-first-crash \
+    --output-spaces MNI152NLin2009cAsym T1w func \
+    --use-syn-sdc \
+    --nprocs 12
+
+## PARALLEL PROCESSING
+
+# Open screen
+screen -L -Logfile $workFolder/log-terminal-vpmb-sub05.txt -S vpmb05
+
+# Start
+#fmriprep-docker ...
+
+# Detach screen
+# Ctrl-A e depois D
