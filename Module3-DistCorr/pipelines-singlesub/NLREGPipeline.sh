cd5dc242bea07de71ded78ccc63a7bb46e341f72
Alexandre Sayal <alexandresayal@sim01.icnas.uc.pt>
2021-10-14 19:09:11 +0000

    nlreg stuff

--- Module3-DistCorr/pipelines-singlesub/NLREGPipeline.sh
+++ Module3-DistCorr/pipelines-singlesub/NLREGPipeline.sh
@@ -81,22 +81,22 @@ bet $WD/func01 $WD/func01_brain -f 0.6 -m
 # Erode mask
 fslmaths $WD/func01_brain_mask -ero $WD/func01_brain_mask
 
-# Check
+# Check brain extraction
 fslview_deprecated $WD/func01 $WD/func01_brain &
 
 # --------------------------------------------------------------------------------
 #  Invert T1w_down to match func01 intensities
 # --------------------------------------------------------------------------------
 
-# Intensity range T1W
-range_t1w=`fslstats $t1Dir/DOWN/${subID}_T1W_down_brain_restore.nii.gz -k $t1Dir/DOWN/${subID}_T1W_down_brain_mask.nii.gz -R`
+# Retrieve intensity range of downsampled T1W
+range_t1w=`fslstats $t1Dir/DOWN/${subID}_T1W_down_brain_restore.nii.gz -k $t1Dir/DOWN/${subID}_T1W_down_brain_mask.nii.gz -R -X -x`
 range_t1w=(${range_t1w// / }) # split into array
 
-# Intensity range func01
+# Retrieve intensity range of func01
 range_func01=`fslstats $WD/func01_brain.nii.gz -k $WD/func01_brain_mask.nii.gz -R -X`
 range_func01=(${range_func01// / }) # split into array
 
-# Auxialiry math
+# Auxiliary math (what to multiply and add to the t1 image)
 mul=`echo "-(${range_func01[1]} - ${range_func01[0]}) / (${range_t1w[1]} - ${range_t1w[0]})" | bc -l`
 
 aux=`echo "${range_t1w[1]} * $mul" | bc -l`
@@ -104,46 +104,69 @@ aux=${aux#-}
 
 add=`echo "$aux + ${range_func01[0]}" | bc -l`
 
-# Invert and mask
-fslmaths $t1Dir/DOWN/${subID}_T1W_down_brain_restore.nii.gz \
+# Invert and mask T1w
+fslmaths $t1Dir/DOWN/${subID}_T1W_down_restore.nii.gz \
         -mul $mul \
         -add $add \
         $WD/T1W_down_inv
 
 fslmaths $WD/T1W_down_inv.nii.gz \
     -mas $t1Dir/DOWN/${subID}_T1W_down_brain_mask \
-    $WD/T1W_down_inv.nii.gz
+    $WD/T1W_down_inv_brain.nii.gz
 
-# Check side by side
-fslview_deprecated $WD/T1W_down_inv.nii.gz &
+# Check T1w and func side by side
+fslview_deprecated $WD/T1W_down_inv_brain.nii.gz &
 fslview_deprecated $WD/func01.nii.gz &
 
-2T1W_down.nii.gz $WD/ants_outputWarpedImage.nii.gz &
+#2T1W_down.nii.gz $WD/ants_outputWarpedImage.nii.gz &
+
+# Rigid registration
+antsRegistrationSyN.sh \
+    -d 3 \
+    -f $WD/T1W_down_inv_brain.nii.gz \
+    -m $WD/func01_brain.nii.gz \
+    -o $WD/func01_brain2T1w_down_inv_brain_ \
+    -t 'a'
+
+# Check affine
+fslview_deprecated $WD/T1W_down_inv_brain.nii.gz $WD/func01_brain2T1w_down_inv_brain_Warped.nii.gz &
 
 # Non linear registration (+ rigid)
 
 antsRegistration \
+    --collapse-output-transforms 1 \
     --dimensionality 3 \
-    --transform Rigid[0.1] \
-    --metric MI[$WD/T1W_down_inv.nii.gz,$WD/func01_brain.nii.gz,1,32,Regular,0.25] \
-    --convergence [1000x500x250x100,1e-6,10] \
-    --shrink-factors 8x4x2x1 \
-    --smoothing-sigmas 3x2x1x0vox \
-    --transform SyN[0.1,3,0] \
-    --metric CC[$WD/T1W_down_inv.nii.gz,$WD/func01_brain.nii.gz,1,3] \
-    --convergence [70x50x20,1e-6,10] \
-    --shrink-factors 4x2x1 \
-    --smoothing-sigmas 2x1x0.5vox \
-    --restrict-deformation 0x1x0 \
+    --float 1 \
+    --initialize-transforms-per-stage 0 \
+    --interpolation Linear \
     --output [$WD/ants_outputTransform_,$WD/ants_outputWarpedImage.nii.gz] \
+    --transform SyN[0.8,2.0,2.0] \
+    --metric Mattes[$WD/T1W_down_inv_brain.nii.gz,$WD/func01_brain2T1w_down_inv_brain_Warped.nii.gz,1,3] \
+    --convergence [100x50,1e-8,20] \
+    --smoothing-sigmas 1.0x0.0vox \
+    --shrink-factors 2x1 \
     --use-estimate-learning-rate-once 1 \
     --use-histogram-matching 1 \
-    --interpolation BSpline \
-    --verbose 1
+    --restrict-deformation 0x1x0 \
+    --transform SyN[0.8,2.0,2.0] \
+    --metric CC[$WD/T1W_down_inv_brain.nii.gz,$WD/func01_brain2T1w_down_inv_brain_Warped.nii.gz,1,3] \
+    --convergence [20x10,1e-8,10] \
+    --smoothing-sigmas 1.0x0.0vox \
+    --shrink-factors 1x1 \
+    --use-estimate-learning-rate-once 1 \
+    --use-histogram-matching 1 \
+    --restrict-deformation 0x1x0 \
+    --winsorize-image-intensities [ 0.001, 1.0 ] \
+    --write-composite-transform 0
+
 
 # Check
-fslview_deprecated $WD/T1W_down_inv.nii.gz $WD/ants_outputWarpedImage.nii.gz &
+fslview_deprecated $WD/T1W_down_inv_brain.nii.gz $WD/ants_outputWarpedImage.nii.gz &
 
+# Test normal antsRegistrationSyN
+antsRegistrationSyN.sh -d 3 -f $WD/T1W_down_inv_brain.nii.gz -m $WD/func01_brain.nii.gz -o $WD/testAntsSyN_ -t 's'
+
+fslview_deprecated $WD/T1W_down_inv_brain.nii.gz $WD/testAntsSyN_Warped.nii.gz &
 
 ## Warp conversion test
 # https://www.mail-archive.com/hcp-users@humanconnectome.org/msg05795.html
