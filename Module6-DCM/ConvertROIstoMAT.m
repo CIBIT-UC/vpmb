190404b7ebb224ca6e7fbb2fa2eb11bb63baa857
Alexandre Sayal <alexandresayal@gmail.com>
2022-11-24 18:57:22 +0000

    init dcm

---
+++ Module6-DCM/ConvertROIstoMAT.m
@@ -0,0 +1,12 @@
+
+
+
+clear matlabbatch
+
+matlabbatch{1}.spm.util.voi.spmmat = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.adjust = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.session = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.name = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.roi{1}.mask.image = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.roi{1}.mask.threshold = 0.5;
+matlabbatch{1}.spm.util.voi.expression = 'i1';
